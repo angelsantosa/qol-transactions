3c5cdb703544de7b3dccc51578c2ac2196b18c6b
Angel Santos <angel_santos@outlook.com>
2025-01-13 06:36:26 -0300

    web clietn?

--- app/db/index.ts
+++ app/db/index.ts
@@ -1,10 +1,10 @@
 import { drizzle } from 'drizzle-orm/libsql';
+import { createClient } from '@libsql/client/web';
 
 export const getDb = () => {
-  return drizzle({
-    connection: {
-      url: process.env.DATABASE_URL ?? '',
-      authToken: process.env.DATABASE_AUTH_TOKEN ?? '',
-    },
+  const client = createClient({
+    url: process.env.DATABASE_URL ?? '',
+    authToken: process.env.DATABASE_AUTH_TOKEN ?? '',
   });
+  return drizzle({ client });
 };
