d39f241824e40c1279d345c1833fff0502ebe594
Angel Santos <angel_santos@outlook.com>
2025-01-13 05:38:34 -0300

    do not let anthropic infer process

--- app/repositories/llm-services.ts
+++ app/repositories/llm-services.ts
@@ -7,7 +7,9 @@ import Anthropic from '@anthropic-ai/sdk';
 export const suggestTransactionDescription = async (
   params: DescriptionPromptParams,
 ) => {
-  const anthropic = new Anthropic();
+  const anthropic = new Anthropic({
+    apiKey: process.env.ANTHROPIC_API_KEY,
+  });
   const msg = await anthropic.messages.create({
     model: 'claude-3-5-sonnet-20241022',
     max_tokens: 30,
