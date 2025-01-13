7cccbe86ac8cf0ab8a8ce992fcb7e65e7856955c
Angel Santos <angel_santos@outlook.com>
2025-01-13 05:28:57 -0300

    adding wrangler and cloudflare stuff

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,12 @@
 import path from 'node:path';
 import { defineConfig } from '@tanstack/start/config';
+import { cloudflare } from 'unenv';
 
 export default defineConfig({
+  server: {
+    preset: 'cloudflare-pages',
+    unenv: cloudflare,
+  },
   vite: {
     resolve: {
       alias: {
